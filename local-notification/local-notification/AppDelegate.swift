516f5e16384ddf04bd018930f7f73a894c2816f8
Renan Kosicki <renankosicki@gmail.com>
2015-09-26 21:09:37 -0300

    Project finished

--- local-notification/local-notification/AppDelegate.swift
+++ local-notification/local-notification/AppDelegate.swift
@@ -15,8 +15,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        if let options = launchOptions {
+            if let notification = options[UIApplicationLaunchOptionsLocalNotificationKey] as? UILocalNotification {
+                if let userInfo = notification.userInfo {
+                    let customField = userInfo["CustomField1"] as! String
+                    // do something neat here
+                }
+            }
+        }
+        
         return true
+    }
+    
+    func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
+        if let userInfo = notification.userInfo {
+            let customField = userInfo["CustomField1"] as! String
+            print("didReceiveLocalNotification: \(customField1)")
+        }
     }
 
     func applicationWillResignActive(application: UIApplication) {
